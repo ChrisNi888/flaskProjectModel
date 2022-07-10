e8573029bd303bbef26068708126713c0f156f00
ChrisNi <545335508@qq.com>
2022-07-10 17:15:43 +0800

    hot-fix commit1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World! hot-fix'
+    return 'Hello World! hot-fix1'
 
 
 if __name__ == '__main__':
